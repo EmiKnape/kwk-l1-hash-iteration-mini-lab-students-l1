77d1e2468caed3fffbcb73b08edde4cd2fa20685
EmiKnape <enknape@aol.com>
2018-06-06 21:46:21 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -4,6 +4,7 @@ character_names = ["Monica", "Joey", "Phoebe"]
 characteristics = ["Neat Freak", "Jokester", "Free Spirit"]
 
 friends_characters = {}
+
 index = 0 
 character_names.each do |name|
   friends_characters[name] = characteristics[index]
