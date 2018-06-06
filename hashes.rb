ca34947f06a3267a0c5b81b6c38b99e9e2db86a3
EmiKnape <enknape@aol.com>
2018-06-06 21:40:54 +0000

    automatically backed up by learn

---
+++ hashes.rb
@@ -0,0 +1,10 @@
+#hash = {"cheese" => "the stuff on pizza"}
+
+characteristics = ["Neat Freak", "Jokester", "Free Spirit"]
+characters = ["Monica", "Joey", "Phoebe"]
+
+friends_characters = {}
+
+characteristics.each do |name|
+  
+end 
