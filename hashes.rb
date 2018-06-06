a553434002b0cc6ce1efca8aaa5d0ae80efdbc2c
EmiKnape <enknape@aol.com>
2018-06-06 21:45:00 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -1,10 +1,11 @@
 #hash = {"cheese" => "the stuff on pizza"}
 
 characteristics = ["Neat Freak", "Jokester", "Free Spirit"]
-characters = ["Monica", "Joey", "Phoebe"]
+character_names = ["Monica", "Joey", "Phoebe"]
 
 friends_characters = {}
 index = 0 
-characteristics.each do |name|
-  friends_characters[name] = characters 
+character_names.each do |name|
+  friends_characters[name] = characteristics[index]
+  index += 1
 end 
